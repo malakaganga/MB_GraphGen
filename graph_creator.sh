8714d2b34c2ff123d733f65f8e21d71346743b55
malakaganga <malakaganga.ac@gmail.com>
2015-11-29 22:26:47 +0530

    tool to generate graphs in terminal and save in .png format

--- graph_creator.sh
+++ graph_creator.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-#=============================================================================
-# Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-#
-#   WSO2 Inc. licenses this file to you under the Apache License,
-#   Version 2.0 (the "License"); you may not use this file except
-#   in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing,
-#   software distributed under the License is distributed on an
-#   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#   KIND, either express or implied. See the License for the
-#   specific language governing permissions and limitations
-#   under the License.
-#=============================================================================
+
 INPUT_FILES=PATH-TO-PROJECT-FOLDER/MB_GraphGen/Texts/*          #Path for the directory where text files are stored/*
 DATAFILES=PATH-TO-PROJECT-FOLDER/MB_GraphGen/DataFiles/   #Path for the directory where data files should be stored/
 GRAPHS=PATH-TO-PROJECT-FOLDER/MB_GraphGen/Graphs/         #Path for the directory where .png files should be stored/
